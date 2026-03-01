155341c7c438f01a6253f4345b346ad2e84f162a
Jason Lantz <jason@muselab.com>
2026-02-28 18:27:29 -0600

    Fix wasm builds

--- src/graph/wasm.rs
+++ src/graph/wasm.rs
@@ -9,12 +9,6 @@
 use super::{export, render, RefGraph};
 use wasm_bindgen::prelude::*;
 
-/// Initialize panic hook for better error messages in the browser console.
-#[wasm_bindgen(start)]
-pub fn init() {
-    console_error_panic_hook::set_once();
-}
-
 // ============================================================================
 // Graph building
 // ============================================================================
